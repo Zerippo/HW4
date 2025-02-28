c8db396b6fb4abf93e8dea40e14e5c996b8ad0ce
Joshua Kao <163955972+Zerippo@users.noreply.github.com>
2025-02-28 11:41:11 -0600

    Update HashingProblems.java

--- HashingProblems.java
+++ HashingProblems.java
@@ -32,17 +32,16 @@ class HashingProblems {
      */
 
     public double getAverage(HashMap<Integer, Integer> map, int[] array) {
-
-        /*
-         * ADD YOUR CODE HERE - DO NOT FORGET TO ADD YOU NAME AT TOP OF FILE
-         *
-         * Note: if NO values found in common between the HashMap and supplied array,
-         * returning 0.0 is NOT correct, as that is not the average value. Whereas
-         * returning 0.0/0.0 IS correct (which would return a non-number).
-         */
-
-         return 0.0 / 0.0;
-  }
+        int sum = 0;
+        int count = 0;
+        for (int key : array) {
+            if (map.containsKey(key)) {
+                sum += map.get(key);
+                count++;
+            }
+        }
+        return count == 0 ? Double.NaN : (double) sum / count;
+    }
 
 
     /*
@@ -53,18 +52,14 @@ class HashingProblems {
      */
 
   public ArrayList<String> odd(HashMap<Integer, String> map) {
-    
-      ArrayList<String> result = new ArrayList<>();
-
-      /*
-       * ADD YOUR CODE HERE
-       *
-       * Hint: Consider iterating over the HashMap using the keySet method.
-       */
-
-
-      return result;
-  }
+        ArrayList<String> result = new ArrayList<>();
+        for (int key : map.keySet()) {
+            if (key % 2 != 0) {
+                result.add(map.get(key));
+            }
+        }
+        return result;
+    }
 
 
   /*
@@ -105,12 +100,18 @@ class HashingProblems {
    */
 
   public int twoSums(int[] numbers, int k) {
-
-      /*
-       * ADD YOUR CODE HERE
-       */
-
-      return -1;
-  }
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+        for (int number : numbers) {
+            if (set.contains(number - k)) {
+                count++;
+            }
+            if (set.contains(number + k)) {
+                count++;
+            }
+            set.add(number);
+        }
+        return count;
+    }
 
 } /* end class HashingProblems */
